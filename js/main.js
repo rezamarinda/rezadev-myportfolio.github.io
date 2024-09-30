7cd66db4ed17aac3f0b68fe342f1318c52ceea71
rezamarinda <rezamarinda17@gmail.com>
2024-09-30 18:19:23 +0900

    perbaikan pada bagian footer

--- js/main.js
+++ js/main.js
@@ -43,19 +43,6 @@
     }
   });
 
-  // Back to top button
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 300) {
-      $('.back-to-top').fadeIn('slow');
-    } else {
-      $('.back-to-top').fadeOut('slow');
-    }
-  });
-  $('.back-to-top').click(function () {
-    $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
-    return false;
-  });
-
   // Typed Initiate
   if ($('.typed-text-output').length == 1) {
     var typed_strings = $('.typed-text').text();
